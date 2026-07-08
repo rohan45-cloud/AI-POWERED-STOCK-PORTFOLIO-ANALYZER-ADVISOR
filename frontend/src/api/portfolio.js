17fe94a279d4e0d7a9e21e6e2fd57c4e8ed11dd9
Rohan Sharma <rohansharmaxyz45@gmail.com>
2026-07-08 22:21:55 +0530

    updated project

---
+++ frontend/src/api/portfolio.js
@@ -0,0 +1,21 @@
+import api from "./client.js";
+
+export const portfolioApi = {
+    getPortfolio: () => api.get("/portfolio").then((res) => res.data),
+    addHolding: (data) =>
+        api.post("/portfolio/holdings", data).then((res) => res.data),
+    updateHolding: (id, data) =>
+        api.patch(`/portfolio/holdings/${id}`, data).then((res) => res.data),
+    deleteHolding: (id) =>
+        api.delete(`/portfolio/holdings/${id}`).then((res) => res.data),
+    sellHolding: (id, data) =>
+        api.post(`/portfolio/holdings/${id}/sell`, data).then((res) => res.data),
+    getTransactions: (symbol) =>
+        api
+        .get("/portfolio/transactions", { params: symbol ? { symbol } : {} })
+        .then((res) => res.data),
+    getPerformance: (days = 90) =>
+        api
+        .get("/portfolio/performance", { params: { days } })
+        .then((res) => res.data),
+};
