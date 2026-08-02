7782fcc500fbeea05e8863d658d344098397bdfe
Rohan Sharma <rohansharmaxyz45@gmail.com>
2026-08-02 12:10:28 +0530

    updated project

--- frontend/src/api/watchlist.js
+++ frontend/src/api/watchlist.js
@@ -1,8 +1,4 @@
 import api from "./client.js";
-<<<<<<< HEAD
-=======
-
->>>>>>> 7e8e71fb96d264e4c700db1e196f0c9b90176e58
 export const watchlistApi = {
     getWatchlist: () => api.get("/watchlist").then((res) => res.data),
     addToWatchlist: (data) => api.post("/watchlist", data).then((res) => res.data),
